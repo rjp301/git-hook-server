a8b39e95ec54f17558d9473f1b553e12bfdeb93c
Riley Paul <rileypaul96@gmail.com>
2023-06-10 06:40:10 -0700

    add bodyparser

--- server.ts
+++ server.ts
@@ -1,17 +1,21 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ version: "1.0.0" });
 });
 
 app.post("/", (req: Request, res: Response) => {
-  console.log(req.body)
-})
+  console.log(req.body);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
