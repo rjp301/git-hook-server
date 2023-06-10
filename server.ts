1e4f8dff79d3ab6a0b1cf21e2b69ee9eaa23eea9
Riley Paul <rileypaul96@gmail.com>
2023-06-10 06:37:30 -0700

    add post

--- server.ts
+++ server.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.json({ version: "1.0.0" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
